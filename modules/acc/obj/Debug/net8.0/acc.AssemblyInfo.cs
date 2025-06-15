f632bed4b57cc2ffc509522dffa689c2e6e99884
Xingxin-Lliao <xingxin.liao@alumnos.upm.es>
2025-06-15 14:25:45 +0200

    Add my acc  files

--- modules/acc/obj/Debug/net8.0/acc.AssemblyInfo.cs
+++ modules/acc/obj/Debug/net8.0/acc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61368ed4f5e52cc76822dcb44ea33a861a86dfee")]
 [assembly: System.Reflection.AssemblyProductAttribute("acc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
